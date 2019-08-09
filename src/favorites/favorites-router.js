739c4b191f6805944e75fb270ad62a52a7152eb2
Arpan Patel <adp8ke@gmail.com>
2019-08-09 11:02:05 -0400

    delete working client side

--- src/favorites/favorites-router.js
+++ src/favorites/favorites-router.js
@@ -54,28 +54,22 @@ favoritesRouter
         })
         .catch(next)
     })
+    
+    
+favoritesRouter
+    .route('/:parkId')
     .delete((req, res, next) =>{
-
-        const {park_id} = req.body
-        const removedFavorite = {user_id: req.user.id, park_id}
-
-        FavoritesService.removeFavorite(
-            req.app.get('db'),
-            removedFavorite
-        )
-            .then(numRowsAffected =>{
-                res.status(204).end()
-            })
-            .catch(next)
+        
+    FavoritesService.removeFavorite(
+        req.app.get('db'),
+        req.params.parkId
+    )
+    .then(numRowsAffected =>{
+        res.status(204).end()
+    })
+    .catch(next)
     })
 
-    // FavoritesService.removeFavorite(
-    //     req.app.get('db'),
-    //     req.params.parkId
-    // )
-    // .then(numRowsAffected =>{
-    //     res.status(204).end()
-    // })
     
     
   
