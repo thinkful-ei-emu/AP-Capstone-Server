18d094dc876f2cda17142b961e8c155c7c24698b
Arpan Patel <adp8ke@gmail.com>
2019-10-09 14:25:03 -0400

    ready for redeploy

--- test/parks-endpoints.spec.js
+++ test/parks-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Parks Endpoints', function() {
+describe('Parks Endpoints', function() {
   let db
 
   const {
